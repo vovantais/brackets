e97f27d1691f7e2dfadec10cee6abfa089a1da54
vovantais <vladimir0297@mail.ru>
2020-10-10 22:53:18 +0300

    init:start and finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let newStr = str.split('');
+	let newArr = [];
+	let arr = [];
+
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		newArr[bracketsConfig[i][0]] = bracketsConfig[i][1];
+	}
+
+	for (let i = 0; i < newStr.length; i++) {
+		if (newStr[i] === arr[arr.length - 1]) {
+			arr.pop();
+		} else {
+			if (newArr[newStr[i]] !== undefined) arr.push(newArr[newStr[i]]); else return false;
+		}
+	}
+
+	return arr.length == 0;
 }
